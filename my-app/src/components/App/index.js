44a292a139a8d47be1f9c417070b82fb67302abf
Hana <hanaadhami01@gmail.com>
2020-02-27 21:14:28 -0800

    react and material ui setup

--- my-app/src/components/App/index.js
+++ my-app/src/components/App/index.js
@@ -1,27 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/core/styles'
 
-import theme from '../utils/themeutil'
+import theme from '../../utils/themeutil'
+import HomePage from '../pages/HomePage'
+import LoginPage from '../pages/LoginPage'
 
 const App = () => {
-    <ThemeProvider theme={theme} className= 'App'>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <ThemeProvider theme={ theme }>
+      <Switch>
+        <Route exact path='/' component={ HomePage } />
+        <Route exact path='/login' component={ LoginPage } />
+        {/* <Route exact path='/signup' component={ SignupPage } />
+        <Route exact path='/details' component={ DetailsPage } /> */}
+      </Switch>
     </ThemeProvider>
+  </Router>
 }
 
 export default App;
