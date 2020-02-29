009462f35bbe003e0d55aa1467981ffc8b5b1b9a
Hana <hanaadhami01@gmail.com>
2020-02-29 13:32:39 -0800

    fixed the boilerplate for material and added a routes folder

---
+++ client/src/components/common/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link as RouterLink } from 'react-router-dom'
+
+const Link = ({ children, to, type = 'internal'}) => (
+    type === 'internal'
+    ? <RouterLink to={ to }>{ children }</RouterLink>
+    :
+)
+
+export default Link
---
+++ client/src/components/pages/HomePage/index.js
@@ -0,0 +1,4 @@
+const HomePage = () => {
+	
+}
+export default HomePage;
