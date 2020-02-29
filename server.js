fd24302b84ee3e012d6503128348248333f71201
Hana <hanaadhami01@gmail.com>
2020-02-29 11:42:28 -0800

    boilerplate

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require("express");
-
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds141118.mlab.com:41118/heroku_jkcd7ftl");
 
-// Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
