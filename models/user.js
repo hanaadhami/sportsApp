de526d317c7feda45819923c6b5cab10ce1d84d1
Blake Fogle <foglebd@gmail.com>
2020-02-29 13:26:23 -0800

    passport.js user authentification/login system implemented

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -9,6 +10,10 @@ const userSchema = new Schema({
   password: {
     type: String, 
     required: true
+  },
+  email: {
+    type: String,
+    index: { unique: true }
   },
   comments: [
     {
@@ -22,6 +27,37 @@ const userSchema = new Schema({
   },
   description: String, 
   favoriteSport: []
+});
+
+/**
+ * Compare the passed password with the value in the database. A model method.
+ *
+ * @param {string} password
+ * @returns {object} callback
+ */
+UserSchema.methods.comparePassword = function comparePassword(password, callback) {
+  bcrypt.compare(password, this.password, callback);
+};
+
+UserSchema.pre('save', function saveHook(next) {
+  const user = this;
+
+  // proceed further only if the password is modified or the user is new
+  if (!user.isModified('password')) return next();
+
+
+  return bcrypt.genSalt((saltError, salt) => {
+    if (saltError) { return next(saltError); }
+
+    return bcrypt.hash(user.password, salt, (hashError, hash) => {
+      if (hashError) { return next(hashError); }
+
+      // replace a password string with hash value
+      user.password = hash;
+
+      return next();
+    });
+  });
 });
 
 const User = mongoose.model("User", userSchema);
