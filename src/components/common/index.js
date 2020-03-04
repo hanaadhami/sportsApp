84f9fc569556f7ce667f1c502ea6267a150d8d24
Holly <holly.blome@gmail.com>
2020-03-03 19:02:14 -0800

    project activities

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link as RouterLink } from 'react-router-dom'
 
-const Link = ({ children, to, type = 'internal'}) => (
+const Link = ({ children, to, type = 'internal' }) => (
     type === 'internal'
     ? <RouterLink to={ to }>{ children }</RouterLink>
     :
