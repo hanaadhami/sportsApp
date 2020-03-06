8a6f62b1e45c3e1a807a6f6ddbd3b5f04e1addef
Blake Fogle <foglebd@gmail.com>
2020-03-05 20:17:23 -0800

    modules updated

---
+++ client/build/precache-manifest.db50244fa530d161013639cebc5de1b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc170fd12c13d89c7a8c22176642a354",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5962897b69a9d975ac79",
+    "url": "/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "46abff4b90389c9881d2",
+    "url": "/static/js/2.51a6336e.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.51a6336e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5962897b69a9d975ac79",
+    "url": "/static/js/main.e04f08f0.chunk.js"
+  },
+  {
+    "revision": "226fd4a5de4b477d8cce",
+    "url": "/static/js/runtime-main.6ff082a3.js"
+  }
+]);
